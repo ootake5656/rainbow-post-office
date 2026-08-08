bcd68714f2e9f160f173107e45727cde2e287b5b
ootake5656 <negishiaoi@gmail.com>
2026-08-08 17:36:41 +0900

    add: ユーザー登録の入力チェックを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
 
   has_many :pets, dependent: :destroy
   has_many :letters, through: :pets
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true, on: :create  # confirmation: trueをつけることで
+  validates :password_confirmation, presence: true, on: :create         # 下の 属性名_confirmation と比較する
+  # on: :create は「新しくデータを作成するときだけ」実行する
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,7 @@
-require "test_helper"
+require "test_helper"   # Railsテストを始めるための準備
 
 class UserTest < ActiveSupport::TestCase
+  # 「Userは複数のPetを持つ」ことの確認
   test "has many pets" do
     association = User.reflect_on_association(:pets)
 
@@ -8,10 +9,66 @@ class UserTest < ActiveSupport::TestCase
     assert_equal :destroy, association.options[:dependent]
   end
 
+  # 「UserはPet経由でLetterを持つ」ことの確認
   test "has many letters through pets" do
     association = User.reflect_on_association(:letters)
 
     assert_equal :has_many, association.macro
     assert_equal :pets, association.options[:through]
+  end
+
+  # 「２つのパスワードが一致する」ことの確認
+  test "password and confirmation must match" do
+  user = User.new(
+    email: "confirmation@example.com",
+    password: "password",
+    password_confirmation: "different"
+  )
+
+  assert_not user.valid?
+  assert_includes user.errors.details[:password_confirmation],
+                  { error: :confirmation, attribute: "Password" }
+  end
+
+  # 「メールアドレスは必須」の確認
+  test "email must be present" do
+  user = User.new(
+    password: "password",
+    password_confirmation: "password"
+  )
+
+  assert_not user.valid?
+  assert_includes user.errors.details[:email],
+                  { error: :blank }
+  end
+
+  # 「メールアドレスは重複不可」の確認
+  test "email must be unique" do
+    User.create!(
+      email: "duplicate@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    duplicate_user = User.new(
+      email: "duplicate@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    assert_not duplicate_user.valid?
+    assert duplicate_user.errors.of_kind?(:email, :taken)
+  end
+
+  # 「新規登録時のパスワードは必須」の確認
+  test "password must be present when creating a user" do
+  user = User.new(
+    email: "password@example.com",
+    password_confirmation: ""
+  )
+
+  # 「パスワードが空のユーザーは登録できないこと」を確認するテスト
+  assert_not user.valid?
+  assert user.errors.of_kind?(:password, :blank)
   end
 end
