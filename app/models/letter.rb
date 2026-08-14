41b66028f42f70e7ef9d76b05aa59bf3db45dc18
ootake5656 <negishiaoi@gmail.com>
2026-08-14 18:06:07 +0900

    add: 一定時間後に返事を表示する判定を実装

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -3,4 +3,16 @@ class Letter < ApplicationRecord
   has_one :reply, dependent: :destroy
 
   validates :content, presence: true, on: :confirmation
+
+  def reply_available_at
+    return unless sent_at
+
+    sent_at + Rails.application.config.x.reply_wait_time
+  end
+
+  def reply_available?
+    return false unless reply && reply_available_at
+
+    Time.current >= reply_available_at
+  end
 end
