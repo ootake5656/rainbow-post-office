17bb1276affff8bf507edeb9f354472876d81970
ootake5656 <negishiaoi@gmail.com>
2026-08-15 00:45:31 +0900

    add: 管理人からの手紙表示画面を実装

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :replies, only: :show
+
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
